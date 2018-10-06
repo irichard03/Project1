ffcbc92d1474d32f27ff7302393d3533df6b4b98
Ian Richard <ianrichard03@gmail.com>
2018-10-05 22:53:52 -0500

    added comments.

--- assets/javascript/fight.js
+++ assets/javascript/fight.js
@@ -1,3 +1,6 @@
+//var to hold damage placeholder for testing, actual damage should be passed
+//in as an argument to the attack functions below.
+var damagePlaceHolder = 20;
 
 //On ready function, do stuff when page loads.
 $(document).ready(function(){
@@ -7,6 +10,14 @@ $(document).ready(function(){
 });    
 
 
+
+
+
+//Button Functions to move player and cpu back/forward by switching float values.
+// class playerForward, playerBackward
+// class cpuForward, cpuBackward
+//  Player                  CPU
+// Back[] []Forward    Forward[]   []Back
 $('.playerForward').click(function(){
     $('.playerFighter').css("float", "right");
 });
@@ -22,3 +33,4 @@ $('.cpuForward').click(function(){
 $('.cpuBackward').click(function(){
     $('.cpuFighter').css("float", "left");
 });
+
