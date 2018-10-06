4ffea56e7a2d495e2fe330775825291f65b00b5f
Ian Richard <ianrichard03@gmail.com>
2018-10-05 22:08:43 -0500

    added player image variety applied gradient to bars.

--- assets/javascript/fight.js
+++ assets/javascript/fight.js
@@ -7,4 +7,18 @@ $(document).ready(function(){
 });    
 
 
+$('.playerForward').click(function(){
+    $('.playerFighter').css("float", "right");
+});
 
+$('.playerBackward').click(function(){
+    $('.playerFighter').css("float", "left");
+});
+
+$('.cpuForward').click(function(){
+    $('.cpuFighter').css("float", "right");
+});
+
+$('.cpuBackward').click(function(){
+    $('.cpuFighter').css("float", "left");
+});
